3d9af68d3ed34503f62962846eacb59e3754b5b8
Yugge <yugge@ludd.ltu.se>
2014-06-28 16:24:50 +0200

    DONOTUSE WIP

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,9 @@ angular.module('app', ['ngAnimate'])
 .controller('mainController', function($scope, $timeout){
     $scope.counter = 0;
     $scope.columns = [];
+
+    $scope.headers = ['Radical Rebrand','Remix Galore','Forgotten baddies','Storytime','Memes from Games','People in games']
+    $scope.questions = [['This is the european name for starfox (SNES)','This is the european name for the contra franchise','this is the japanese name of our super mario bros 2','d',['e']],[],[],[],[],[]];
     $scope.board = true;
     $timeout(function(){
         $scope.addNumber();
@@ -17,11 +20,13 @@ angular.module('app', ['ngAnimate'])
             console.log('adding',$scope.counter);
         }
     };
-    $scope.showAnswer = function(){
+    $scope.showAnswer = function(column,row){
         $scope.board = false;
         $scope.question = true;
+        $scope.currentAnswer = $scope.questions[column][row];
+        console.log($scope.currentAnswer, column, row);
     };
-
+2
     $scope.showBoard = function(){
         $scope.board = true;
         $scope.question = false;
